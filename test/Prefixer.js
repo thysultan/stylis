a26a6a33d4f010a9f711b701a1080d091c238c63
thysultan <sultantarimo@me.com>
2020-01-14 02:49:27 +0300

    separate prefixer tests from parser tests

---
+++ test/Prefixer.js
@@ -0,0 +1,15 @@
+import {prefix, compile, serialize} from "../index.js"
+
+describe('Prefixer', () => {
+	// TODO:
+	// - test vendor prefixing:
+	//    - https://github.com/thysultan/stylis.js/blob/4561e9bc830fccf1cb0e9e9838488b4d1d5cebf5/tests/spec.js#L553
+	//    - https://github.com/thysultan/stylis.js/blob/4561e9bc830fccf1cb0e9e9838488b4d1d5cebf5/tests/spec.js#L700
+	//    - https://github.com/thysultan/stylis.js/blob/4561e9bc830fccf1cb0e9e9838488b4d1d5cebf5/tests/spec.js#L728
+	//    - https://github.com/thysultan/stylis.js/blob/4561e9bc830fccf1cb0e9e9838488b4d1d5cebf5/tests/spec.js#L778
+	//    - https://github.com/thysultan/stylis.js/blob/4561e9bc830fccf1cb0e9e9838488b4d1d5cebf5/tests/spec.js#L810
+	//    - https://github.com/thysultan/stylis.js/blob/4561e9bc830fccf1cb0e9e9838488b4d1d5cebf5/tests/spec.js#L835
+	//    - https://github.com/thysultan/stylis.js/blob/4561e9bc830fccf1cb0e9e9838488b4d1d5cebf5/tests/spec.js#L876
+	//    - https://github.com/thysultan/stylis.js/blob/4561e9bc830fccf1cb0e9e9838488b4d1d5cebf5/tests/spec.js#L953
+	// - add prefixer API and accompanying tests
+})
