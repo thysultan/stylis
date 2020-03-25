2f078797089a485f72e8ef48d3ebec48eefde5d0
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-03-25 21:25:28 +0100

    Fix issue with @import rules not being passed to the rulesheet plugin

--- test/Middleware.js
+++ test/Middleware.js
@@ -4,8 +4,14 @@ const stack = []
 
 describe('Middleware', () => {
 	test('rulesheet', () => {
-  	serialize(compile(`.user{ h1 {width:0;} @media{width:1;}@keyframes name{from{width:0;}to{width:1;}}}`), middleware([prefixer, stringify, rulesheet(value => stack.push(value))]))
-  	expect(stack).to.deep.equal([`.user h1{width:0;}`, `@media{.user{width:1;}}`, `@-webkit-keyframes name{from{width:0;}to{width:1;}}`, `@keyframes name{from{width:0;}to{width:1;}}`])
+  	serialize(compile(`.user{ h1 {width:0;} @media{width:1;}@keyframes name{from{width:0;}to{width:1;}}}@import url('something.com/file.css');`), middleware([prefixer, stringify, rulesheet(value => stack.push(value))]))
+  	expect(stack).to.deep.equal([
+      `.user h1{width:0;}`,
+      `@media{.user{width:1;}}`,
+      `@-webkit-keyframes name{from{width:0;}to{width:1;}}`,
+      `@keyframes name{from{width:0;}to{width:1;}}`,
+      `@import url('something.com/file.css');`
+    ])
   })
 
   test('namespace', () => {
