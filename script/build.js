9af06e2d5f5b19089281649f2f0b7167458aa396
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-12-02 14:14:22 +0100

    Create commonjs package scope for the package.json#main file

--- script/build.js
+++ script/build.js
@@ -2,6 +2,15 @@ import {join} from 'path'
 import {terser} from 'rollup-plugin-terser'
 import size from 'rollup-plugin-size'
 
+const emitPackageScopeFile = (type) => {
+	return {
+		name: 'emit-package-scope-file',
+		generateBundle() {
+			this.emitFile({ type: 'asset', fileName: 'package.json', source: `{ "type": "${type}" }\n` });
+		}
+	};
+}
+
 const options = {mangle: true, compress: false, toplevel: true}
 const defaults = {
 	onwarn(warning, warn) {
@@ -21,14 +30,8 @@ export default ({configSrc = './', configInput = join(configSrc, 'index.js')}) =
 		{
 			...defaults,
 			input: configInput,
-			output: [{file: join(configSrc, 'dist', 'stylis.js'), format: 'umd', name: 'stylis', freeze: false, sourcemap: true}],
-			plugins: [terser(options), size()]
-		},
-		{
-			...defaults,
-			input: configInput,
-			output: [{file: join(configSrc, 'dist', 'stylis.cjs'), format: 'umd', name: 'stylis', freeze: false, sourcemap: true}],
-			plugins: [terser(options), size()]
+			output: [{file: join(configSrc, 'dist', 'umd', 'stylis.js'), format: 'umd', name: 'stylis', freeze: false, sourcemap: true}],
+			plugins: [emitPackageScopeFile('commonjs'), terser(options), size()]
 		},
 		{
 			...defaults,
