7663d110ff4f95c1b5b1efd78a559724b3d6ef01
Sultan <810601+thysultan@users.noreply.github.com>
2021-12-09 21:51:45 +0300

    Update Serializer.js

--- src/Serializer.js
+++ src/Serializer.js
@@ -25,10 +25,14 @@ export function serialize (children, callback) {
  */
 export function stringify (element, index, children, callback) {
 	switch (element.type) {
-		case IMPORT: case DECLARATION: return element.return = element.return || element.value
-		case COMMENT: return ''
-		case RULESET: element.value = element.props.join(','); break
+		case IMPORT: case DECLARATION:
+			return element.return = element.return || element.value
+		case COMMENT:
+			return ''
+		case RULESET: element.value = element.props.join(',')
+			break
 		case KEYFRAMES: element.return = element.value + '{}'
+			break
 	}
 
 	return strlen(children = serialize(element.children, callback)) ? element.return = element.value + '{' + children + '}' : ''
