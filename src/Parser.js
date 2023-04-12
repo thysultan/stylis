204b35d480355b9cb8297e722e4a97887a37ae05
Sultan <810601+thysultan@users.noreply.github.com>
2023-04-13 02:31:35 +0300

    fix whitespace character unexpected inclusion

--- src/Parser.js
+++ src/Parser.js
@@ -79,7 +79,7 @@ export function parse (value, root, parent, rule, rules, rulesets, pseudo, point
 					// \0 }
 					case 0: case 125: scanning = 0
 					// ;
-					case 59 + offset:
+					case 59 + offset: if (ampersand == -1) characters = replace(characters, /\f/g, '')
 						if (property > 0 && (strlen(characters) - length))
 							append(property > 32 ? declaration(characters + ';', rule, parent, length - 1) : declaration(replace(characters, ' ', '') + ';', rule, parent, length - 2), declarations)
 						break
